e0404b63059ac18ac6af1508b38afde0e6671bc8
IceColt <92539387+IceColt@users.noreply.github.com>
2023-10-16 12:03:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link rel="icon" href="animaxlogo.png">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style> 
+  
+.titre {
+	text-align: center;
+    padding: 5px;
+	font-size: calc(4000vw / 1080);
+	}
+	
+.soustitre {
+	text-align: center;
+    padding: 5px;
+	font-size: calc(2500svw / 1080);
+	
+h1 {
+    text-align: center;
+    font-size: calc(4500vw / 1080);
+	
+   
+  </style>
+  <title>HISHLIBIDISH</title>
+</head>
+	<div class="titre">
+		<h1>Bienvenue sur Hishlibidish</h1>
+	</div>
+	<div class="soustitre">
+		<h2>Un Site D'entre-Aide et de Partage de Fichier</h2>
+	</div>
+	<div class="Upload"> 
+		<a href="/Upload/" ><img src="Uploadv2.png"></a>
+		<a href="/All/" ><img src="fichier.png"></a>
+		<a href="/Lien/" ><img src="link.png"></a>
+		<a href="/All/Application/" ><img src="app.png"></a>	
+				
+	</div>
+	<div class="Upload"> 
+		<a href="/Upload/test.php" ><img src="Uploadtest.php.png"></a>
+		<a href="/All/Manga/" ><img src="mangav2.png"></a>
+	</div>  
+
+<body>
+
+</body>
+
+</html>
